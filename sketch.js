78c94bcca3cb697be5802de503fdc0d1a262bde7
ANDY MILLS <SM21BM@FSU.EDU>
2023-09-18 12:02:28 -0400

    Animation and the Mouse - 1

--- sketch.js
+++ sketch.js
@@ -1,20 +1,25 @@
 let x = 200;
 let speed = 4;
 let diameter = 40;
-
 function setup() {
-  createCanvas(400, 400);}
+  createCanvas(400, 400);
+}
 
 function draw() {
   background("pink");
-  ellipse(x,height/2,40);
   fill("red");
   stroke("noStroke");
+  ellipse(x,height/2,40);
   if (x >= width || x<= 0) {
     speed = speed * -1;
   }
   x += speed;
-  //if ( x<= 0) {
-    //speed = speed * -1;
-  //}
 }
+
+function mouseClicked() {
+  noLoop();
+}
+
+
+  
+
