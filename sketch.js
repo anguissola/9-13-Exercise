b80326834fc9a77a2c6195babccdcafb9304ef18
ANDY MILLS <SM21BM@FSU.EDU>
2023-09-18 20:37:03 -0400

    mess of interest

--- sketch.js
+++ sketch.js
@@ -1,28 +1,59 @@
-let x = 200;
+let startpoint;
+let launchpoint;
+let endpoint;
+let value;
+let clickvalue;
+let r, g, b;
 let speed = 4;
 let diameter = 40;
 function setup() {
   createCanvas(400, 400);
   noLoop();
+  startpoint = 0;
+  launchpoint = 200;
+  endpoint = 400;
+  value=("#7BB2DD");
+  clickvalue=random(r,g,b);
+  r = random(0,255);
+  g = random(0,255);
+  b = random(0,255); 
 }
 
 function draw() {
+
+  //drawing stuff
   background("pink");
-  fill("red");
-  stroke("noStroke");
-  ellipse(x,height/2,40);
-  if (x >= width || x<= 0) {
+  fill(value);
+  stroke(value);
+  // if (mouseIsPressed) {
+  //   fill(r,g,b);
+  // } else {
+  //   fill(255);
+  // }
+  ellipse(launchpoint,200,40);
+
+  //logic
+  launchpoint = launchpoint + 1;
+
+  if (launchpoint >= endpoint || launchpoint<= 0) {
     speed = speed * -1;
   }
-  x += speed;
+  launchpoint += speed;
 }
 
+//mouse-initiated animation
 function mouseClicked() {
   loop();
+
+  if (value === 0) {
+    value = (clickvalue);
+  } else {
+    value = (clickvalue);
+  }
 }
 
 
 
-
-  
-
+// function mouseReleased() {
+//   noLoop();
+// }
