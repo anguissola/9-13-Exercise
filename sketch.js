4c4a9a4676f326d595793dc83888118c143c5482
ANDY MILLS <SM21BM@FSU.EDU>
2023-09-19 17:28:27 -0400

    Animation and the Mouse - Stopping point

--- sketch.js
+++ sketch.js
@@ -1,25 +1,48 @@
 let x = 200;
+let y =200;
 let speed = 4;
 let diameter = 40;
+let outerfill = 100;
+
+var millisecond = millis();
 var canvas;
+var pre;
+var p;
+var purpleballskeepgoing;
+var middlefill;
 
 function setup() {
   createCanvas(400, 400);
-  noLoop();
+  // noLoop();
+  //buffoonery
+  pre = createElement('pre', '                        Goodluck!');
+  pre.position(0,560);
+  middlefill = (100);
 }
 
 function draw() {
   //drawing
-  background("pink");
-  fill("red");
-  stroke("red");
-  ellipse(x,height/2,40);
+  background(205,247,165);
+
+  fill(middlefill);
+  ellipse(x,height/2,diameter);
   //logic
-  x += speed;
+  x = x + speed;
   if (x >= width || x<= 0) {
     speed = speed * -1;
   }
- 
+  //drawing next shapes
+  fill(outerfill, outerfill, 20);
+  ellipse(y,100,diameter);
+  fill(20, outerfill, outerfill);
+  ellipse(y,300,diameter);
+  //logic for next shapes
+  y = y + speed;
+  if (y >= width || y<= 0) {
+    speed = speed * -1;
+  }
+  //logic for top and bottom dot fill
+  outerfill = outerfill + speed;
 }
 
 //animation directed by mouse click
@@ -31,3 +54,28 @@ function mouseClicked() {
   }
   redraw();
 }
+
+function mousePressed() {
+  if (isLooping()) {
+
+  } else {
+  middlefill = (( random(0,255)));
+  }
+} 
+
+
+
+
+//bufoonery
+// function mousePressed() {
+//   createP()
+//   p=createElement('p', "Vexation!");
+//   p.position(500,400);
+// }
+
+
+//notes
+
+  // logic for backrgound change
+
+ //figure - 10000
