d1f8f75c8a2b74394ff6aab17773bdf5b0964423
ANDY MILLS <SM21BM@FSU.EDU>
2023-09-18 21:46:28 -0400

    Animation and the Mouse - 3 (and buffoonery)

--- sketch.js
+++ sketch.js
@@ -1,27 +1,33 @@
 let x = 200;
 let speed = 4;
 let diameter = 40;
+var canvas;
+
 function setup() {
   createCanvas(400, 400);
-
+  noLoop();
 }
 
 function draw() {
+  //drawing
   background("pink");
   fill("red");
   stroke("red");
   ellipse(x,height/2,40);
+  //logic
   x += speed;
   if (x >= width || x<= 0) {
     speed = speed * -1;
   }
  
 }
-noloop()
-function mouseClicked() {
-  speed = 0
-}
 
-function mouseClicked () {
-  speed = 4
+//animation directed by mouse click
+function mouseClicked() {
+  if (isLooping()) {
+    noLoop();
+  } else {
+    loop();
+  }
+  redraw();
 }
